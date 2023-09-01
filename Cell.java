9b67aa1b54478d28b5f14fda88d4d9fc283a91f0
ZhixiaPang <zhixiapang@gmail.com>
2023-09-02 00:39:14 +1200

    Debug CROSS Player and NOUGHT Player

--- Cell.java
+++ Cell.java
@@ -3,43 +3,46 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+
+
 public class Cell {
     //content of this cell (EMPTY, CROSS, NOUGHT)
-	private final Player content;
-	//row and column of this cell
-	private final int row, col;
-	
-	// Constructor to initialise this cell with the specified row and col
-	public Cell(int row, int col) {
-		this.row = row;
-		this.col = col;
-		this.content = Player.EMPTY; // Assign a default value to 'content'
-	}
+    private final Player content;
+    //row and column of this cell
+    private final int row, col;
 
-	public Player getContent() {
-		return content;
-	}
+    // Constructor to initialize this cell with the specified row, col, and content
+    public Cell(int row, int col, Player content) {
+        this.row = row;
+        this.col = col;
+        this.content = content;
+    }
+
+    public Player getContent() {
+        return content;
+    }
 
 
-	//Paint itself on the graphics canvas, given the Graphics context g
-	public void paint(Graphics g) {
-		// Graphics2D allows setting of pen's stroke size
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setStroke(new BasicStroke(GameMain.SYMBOL_STROKE_WIDTH *2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
 
-		//draw the symbol in the position
-		int x1 = col * GameMain.CELL_SIZE + GameMain.CELL_PADDING;
-		int y1 = row * GameMain.CELL_SIZE + GameMain.CELL_PADDING;
+    //Paint itself on the graphics canvas, given the Graphics context g
+    public void paint(Graphics g) {
+        // Graphics2D allows setting of pen's stroke size
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(GameMain.SYMBOL_STROKE_WIDTH *2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
 
-		if (content == Player.CROSS) {
-			g2d.setColor(Color.RED);
-			int x2 = (col + 1) * GameMain.CELL_SIZE - GameMain.CELL_PADDING;
-			int y2 = (row + 1) * GameMain.CELL_SIZE - GameMain.CELL_PADDING;
-			g2d.drawLine(x1, y1, x2, y2);
-			g2d.drawLine(x2, y1, x1, y2);
-		} else if (content == Player.NOUGHT) {
-			g2d.setColor(Color.BLUE);
-			g2d.drawOval(x1, y1, GameMain.SYMBOL_SIZE, GameMain.SYMBOL_SIZE);
-		}
-	}
-	}
+        //draw the symbol in the position
+        int x1 = col * GameMain.CELL_SIZE + GameMain.CELL_PADDING;
+        int y1 = row * GameMain.CELL_SIZE + GameMain.CELL_PADDING;
+
+        if (content == Player.CROSS) {
+            g2d.setColor(Color.RED);
+            int x2 = (col + 1) * GameMain.CELL_SIZE - GameMain.CELL_PADDING;
+            int y2 = (row + 1) * GameMain.CELL_SIZE - GameMain.CELL_PADDING;
+            g2d.drawLine(x1, y1, x2, y2);
+            g2d.drawLine(x2, y1, x1, y2);
+        } else if (content == Player.NOUGHT) {
+            g2d.setColor(Color.BLUE);
+            g2d.drawOval(x1, y1, GameMain.SYMBOL_SIZE, GameMain.SYMBOL_SIZE);
+        }
+    }
+}
